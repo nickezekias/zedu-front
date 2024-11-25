6d1571d1045e2258bcb0eb01485860aee60fba25
Nick Ezekiel <nickezekias@gmail.com>
2024-11-25 15:16:33 +0100

    added and configured i18n with vue-i18n package

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import i18n from './locales/i18n'
 import './assets/main.css'
 
 import { createApp } from 'vue'
@@ -10,5 +11,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(i18n)
 app.mount('#app')

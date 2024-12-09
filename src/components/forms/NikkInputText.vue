c76b44a3f19d5422a0c8e7e5f4d5849c58cf4b24
Nick Ezekiel <nickezekias@gmail.com>
2024-12-09 18:43:37 +0100

    added base form components

---
+++ src/components/forms/NikkInputText.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import FloatLabel from 'primevue/floatlabel'
+import InputText from 'primevue/inputtext'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const model: any = defineModel()
+const props = defineProps<{
+  id: string
+  disabled?: boolean
+  errorHelpLabel: string
+  label: string
+  readonly?: boolean
+  type: string
+  isError: boolean
+}>()
+</script>
+
+<template>
+  <FloatLabel variant="on">
+    <InputText v-model="model" class="w-full p-3 border shadow-none" :disabled="props.disabled" :invalid="props.isError"
+      :id="props.id" :readonly="props.readonly" :type="type" />
+    <label :class="props.isError ? 'nikk-invalid' : ''" :for="props.id">{{ $t(`${props.label}`) }}</label>
+    <small v-if="props.isError" :id="`${props.id}-help`" class="nikk-invalid">
+      {{ $t(`${props.errorHelpLabel}`) }}
+    </small>
+  </FloatLabel>
+</template>
+
+<style scoped>
+.p-inputtext:focus {
+  border-color: var(--p-primary-color)
+}
+
+.p-inputtext.p-invalid {
+  border-color: var(--p-red-500)
+}
+</style>

f1758c5e1918611f36a4037e1167961fca0e468b
Nick Ezekiel <nickezekias@gmail.com>
2024-12-10 23:03:46 +0100

    fixed middleware piplining in router index

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import GuestLayout from '@/layouts/guest/IndexLayout.vue'
 import guest from './middleware/guest.middleware'
 import auth from './middleware/auth.middleware'
 import admin from './middleware/admin.middleware'
+import middlewarePipeline from './middleware/middlewarePipeline'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -38,6 +39,21 @@ const router = createRouter({
       ],
     },
   ],
+})
+
+router.beforeEach(async (to, from, next) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+  const middleware: Array<Function> = to.meta.middleware as Array<Function>
+  const context = { to, from, next }
+
+  if (!middleware) {
+    return next()
+  }
+
+  await middleware[0]({
+    ...context,
+    next: await middlewarePipeline(context, middleware, 1),
+  })
 })
 
 export default router

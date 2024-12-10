90660f92c249b4700eb77f8e535d31059232009b
Nick Ezekiel <nickezekias@gmail.com>
2024-12-10 07:27:41 +0100

    added user avatar component

---
+++ src/components/forms/user/UserAvatarMenu.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import PrimeAvatar from 'primevue/avatar'
+import Menu from 'primevue/menu'
+import { useI18n } from 'vue-i18n';
+import { useAccountStore } from '@/stores/account.store';
+
+const accountStore = useAccountStore()
+const { t } = useI18n()
+
+
+const items = ref([
+  {
+    label: t('labels.logout'),
+    icon: 'pi pi-sign-out',
+    command: async () => { logout() }
+  },
+])
+
+const menu = ref()
+
+const toggle = (event: Event) => {
+  menu.value.toggle(event);
+};
+
+async function logout() {
+  await accountStore.logout()
+}
+</script>
+
+<template>
+  <div>
+    <PrimeButton type="button" icon="pi pi-ellipsis-v" @click="toggle" aria-haspopup="true" aria-controls="overlay_menu"
+      text rounded>
+      <PrimeAvatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png"
+        style="width: 2.2rem; height: 2.2rem" />
+
+      <Menu ref="menu" class="bg-white dark:bg-surface-950" id="overlay_menu" :model="items" :popup="true" />
+    </PrimeButton>
+
+  </div>
+</template>

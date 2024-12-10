7310e03d501c686888d4b3a230270a92a9fd3abc
Nick Ezekiel <nickezekias@gmail.com>
2024-12-10 07:28:03 +0100

    added admin appLayout

---
+++ src/layouts/admin/IndexLayout.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Toast from 'primevue/toast'
+import Navbar from './NavBar.vue';
+</script>
+
+<template>
+  <div>
+    <Toast />
+    <Navbar />
+    <RouterView class="p-2 md:p-4 pb-32 md:pb-2" />
+  </div>
+</template>
---
+++ src/layouts/admin/NavBar.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Toolbar from 'primevue/toolbar'
+import UserAvatarMenu from '@/components/forms/user/UserAvatarMenu.vue';
+</script>
+
+<template>
+  <Toolbar class="py-0 rounded-none border-b">
+    <template #start>
+      <div class="logo">
+        <span class="z">Z</span><span class="edu">edu</span>
+      </div>
+    </template>
+
+    <template #center>
+      <router-link :to="{ name: 'dashboard' }" activeClass="border-b border-primary" class="hidden md:flex py-2">
+        <PrimeButton text plain class="" :label="$t('labels.dashboard')" />
+      </router-link>
+    </template>
+
+    <template #end>
+      <div class="flex align-items-center gap-3">
+        <UserAvatarMenu />
+      </div>
+    </template>
+  </Toolbar>
+</template>

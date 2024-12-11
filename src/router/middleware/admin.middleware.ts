44cb41c55a4f494c4e29d7e5aaa90c0ef5616298
Nick Ezekiel <nickezekias@gmail.com>
2024-12-11 20:11:38 +0100

    fixed admin middleware and added missing locale strings

--- src/router/middleware/admin.middleware.ts
+++ src/router/middleware/admin.middleware.ts
@@ -3,7 +3,7 @@ import { useAccountStore } from '@/stores/account.store'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function admin({ next }: any) {
   const accountStore = useAccountStore()
-  if (accountStore.user?.isAdmin()) {
+  if (accountStore.user && accountStore.user.isAdmin()) {
     next()
   } else {
     next({ name: 'notFound' })
